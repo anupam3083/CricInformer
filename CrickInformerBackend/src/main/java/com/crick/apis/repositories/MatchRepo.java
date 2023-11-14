de8fe41e30a1bac3fd17256a8066ee848d5b45c0
anupam3083 <41772689+anupam3083@users.noreply.github.com>
2023-11-14 19:59:43 +0530

    Add files via upload

---
+++ CrickInformerBackend/src/main/java/com/crick/apis/repositories/MatchRepo.java
@@ -0,0 +1,12 @@
+package com.crick.apis.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.crick.apis.entities.Match;
+
+
+public interface MatchRepo extends JpaRepository<Match, Integer>{
+
+	Optional<Match> findTeamByTeamHeading(String teamHeading);
+}
